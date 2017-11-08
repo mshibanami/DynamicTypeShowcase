fa1c932af3f37a6e37af027fb97fd2f6ae3b2c0e
Manabu Nakazawa <mshibanami+git@gmail.com>
2017-11-08 23:47:07 +0900

    Image

---
+++ DynamicTypeShowcase/Image/ImageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ImageViewController.swift
+//  DynamicTypeShowcase
+//
+//  Created by abc on 2017/11/08.
+//  Copyright © 2017 mshibanami. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
