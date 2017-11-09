0ae586c4480973ab32e4475e53f8f078a3c7d7ca
Manabu Nakazawa <mshibanami+git@gmail.com>
2017-11-09 22:02:33 +0900

    add CustomFontViewController

---
+++ DynamicTypeShowcase/CustomFont/CustomFontViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CustomFontViewController.swift
+//  DynamicTypeShowcase
+//
+//  Created by abc on 2017/11/09.
+//  Copyright © 2017 mshibanami. All rights reserved.
+//
+
+import UIKit
+
+class CustomFontViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
