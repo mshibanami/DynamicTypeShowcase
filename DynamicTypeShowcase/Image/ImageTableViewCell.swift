155a3403ce8a325077ec7130c672622c01956a40
Manabu Nakazawa <mshibanami+git@gmail.com>
2017-11-10 01:32:11 +0900

    Implement Images Scene

---
+++ DynamicTypeShowcase/Image/ImageTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ImageTableViewCell.swift
+//  DynamicTypeShowcase
+//
+//  Created by abc on 2017/11/09.
+//  Copyright © 2017 mshibanami. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var scalableImageView: UIImageView!
+    @IBOutlet weak var originalSizeLabel: UILabel!
+    @IBOutlet weak var currentSizeLabel: UILabel!
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        let imageViewSize = self.scalableImageView.frame.size
+        self.currentSizeLabel.text = "current size: \(imageViewSize.width)x\(imageViewSize.height)"
+    }
+}
--- DynamicTypeShowcase/Image/ImageViewController.swift
+++ DynamicTypeShowcase/Image/ImageViewController.swift
@@ -9,7 +9,38 @@
 import UIKit
 
 class ImageViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    let images = [#imageLiteral(resourceName: "SampleImage1_40x40"), #imageLiteral(resourceName: "SampleImage2_40x40")]
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+extension ImageViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "ImageTableViewCell",
+            for: indexPath)
+            as! ImageTableViewCell
+
+        let img = images[indexPath.row]
+        cell.scalableImageView.image = img
+        cell.originalSizeLabel.text = "original size: \(img.size.width)x\(img.size.height)"
+
+        return cell
+    }
+}
+
+extension ImageViewController: UITableViewDelegate {
+
 }
