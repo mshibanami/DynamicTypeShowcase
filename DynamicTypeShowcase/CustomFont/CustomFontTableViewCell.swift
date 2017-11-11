b5ff76551ba50e3ce86ba7922c444e4d422ccc2a
Manabu Nakazawa <mshibanami+git@gmail.com>
2017-11-11 11:49:48 +0900

    add a sample font for CustomFont

---
+++ DynamicTypeShowcase/CustomFont/CustomFontTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomFontTableViewCell.swift
+//  DynamicTypeShowcase
+//
+//  Created by abc on 2017/11/11.
+//  Copyright © 2017 mshibanami. All rights reserved.
+//
+
+import UIKit
+
+class CustomFontTableViewCell: UITableViewCell {
+    @IBOutlet weak var sampleTextLabel: UILabel!
+    @IBOutlet weak var textStyleNameLabel: UILabel!
+    @IBOutlet weak var fontNameLabel: UILabel!
+    @IBOutlet weak var fontSizeLabel: UILabel!
+}
--- DynamicTypeShowcase/CustomFont/CustomFontViewController.swift
+++ DynamicTypeShowcase/CustomFont/CustomFontViewController.swift
@@ -9,7 +9,44 @@
 import UIKit
 
 class CustomFontViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+extension CustomFontViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "CustomFontTableViewCell",
+            for: indexPath)
+            as! CustomFontTableViewCell
+
+        if #available(iOS 11, *) {
+            let textStyle = UIFontTextStyle.body
+            let fontName = UIFont.familyNames.first!
+            let fontSize: CGFloat = 20.0
+            let font = UIFont(name: fontName, size: fontSize)!
+            let fontMetrics = UIFontMetrics(forTextStyle: textStyle)
+
+            cell.sampleTextLabel.font = fontMetrics.scaledFont(for: font)
+            cell.textStyleNameLabel.text = "TextStyle: \(textStyle.name)"
+            cell.fontNameLabel.text = fontName
+            cell.fontSizeLabel.text = "\(fontSize)pt"
+        }
+
+        return cell
+    }
+}
+
+extension CustomFontViewController: UITableViewDelegate {
+
 }
