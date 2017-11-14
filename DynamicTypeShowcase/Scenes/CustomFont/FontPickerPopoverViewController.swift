cc46870d378de1691531b8221f20603c11964d11
Manabu Nakazawa <mshibanami@gmail.com>
2017-11-14 13:29:33 +0900

    show dummy data for picker

--- DynamicTypeShowcase/Scenes/CustomFont/FontPickerPopoverViewController.swift
+++ DynamicTypeShowcase/Scenes/CustomFont/FontPickerPopoverViewController.swift
@@ -11,5 +11,25 @@ import Reusable
 import KUIPopOver
 
 class FontPickerPopoverViewController: UIViewController, StoryboardBased {
+    @IBOutlet weak var pickerView: UIPickerView!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension FontPickerPopoverViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 3
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️🙆‍♂️"
+    }
+}
+
+extension FontPickerPopoverViewController: UIPickerViewDelegate {
 }
