6a1ba37d4670e3c47f4cf403dc5ce8dd13937ac9
Manabu Nakazawa <mshibanami+git@gmail.com>
2017-11-18 13:35:33 +0900

    workaround for a bug of Dynamic Type for images

--- DynamicTypeShowcase/Scenes/Image/ImageTableViewCell.swift
+++ DynamicTypeShowcase/Scenes/Image/ImageTableViewCell.swift
@@ -21,4 +21,18 @@ class ImageTableViewCell: UITableViewCell, Reusable {
         let imageViewSize = self.scalableImageView.frame.size
         self.currentSizeLabel.text = "current size: \(imageViewSize.width)x\(imageViewSize.height)"
     }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+
+        // HACK: This is a workaround to apply the overrided traitCollection to the imageView.
+        // iOS11 or later supports scaled image with Size Category.
+        // But currently, it looks work fine only for global size category (`UIApplication.preferredContentSizeCategory`)
+        // Not for overrided traitCollection with `UIViewController.setOverrideTraitCollection(_:forChildViewController:)`
+        // I suspect this is a iOS11's bug...
+        // So I wrote this code to avoid it.
+        // We, 3rd party developers, should not call traitCollectionDidChange() manually.
+        // But I do anyway because it works fine for some reasons.
+        self.scalableImageView.traitCollectionDidChange(nil)
+    }
 }
